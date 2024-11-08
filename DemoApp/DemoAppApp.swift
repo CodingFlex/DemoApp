ffca4869c56b89597c39177dc7152d943d17f024
Oluwatunmise Dunmade <oluwatunmisedunmade2017@gmail.com>
2024-11-08 19:45:37 +0100

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by FlexOff on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
